d7944482cb869022078ee33a68358d83c121acf1
George <antoniou@ualberta.ca>
2014-12-07 14:35:42 -0700

    updates project viewer
    -also added a blog post
    -fixed hover issue

--- scripts/index.js
+++ scripts/index.js
@@ -18,19 +18,51 @@ $(window).scroll(function() {
     var about_location = $("#about").position();
     var projects_location = $("#projects").position();
     var header_bottom = $("#home").height();
-    var just_changed = 0;
+    var index = -1;
 
     
-  	if (scroll < about_location.top - 50) {
+  	if (index != 0 && scroll < about_location.top - 50) {
   		document.getElementById("abt").style.color = "white";
         document.getElementById("hme").style.color = "#1597D8";
-  	} else if (scroll > about_location.top - 50 && scroll < projects_location.top - 50) {
+        index = 0;
+  	} else if (index != 1 && scroll > about_location.top - 50 && scroll < projects_location.top - 50 ) {
   		document.getElementById("abt").style.color = "#1597D8";
         document.getElementById("hme").style.color = "white";
         document.getElementById("prj").style.color = "white";
-  	} else if (scroll > projects_location.top - 50){
+        index = 1;
+  	} else if (index != 2 && scroll > projects_location.top - 50){
+        document.getElementById("hme").style.color = "white";
         document.getElementById("abt").style.color = "white";
         document.getElementById("prj").style.color = "#1597D8";
+        index = 2;
     }
     
 });
+
+$(window).load(function() {
+    document.getElementById("viewer").innerHTML = document.getElementById("PRJ0").innerHTML;
+    $("#BUT0").css('color', '#35343B');
+    $("#BUT0").css('background-color', '#FEFAFF');
+});
+
+function updateProjectPreview(ID){
+    var project;
+    switch(ID) {
+        case 0:
+            project = document.getElementById("PRJ0");
+            document.getElementById("viewer").innerHTML = project.innerHTML;
+            $("#BUT0").css('color', '#35343B');
+            $("#BUT0").css('background-color', '#FEFAFF');
+            $("#BUT1").css('color', '#FEFAFF');
+            $("#BUT1").css('background-color', '#35343B');
+            break;
+        case 1:
+            project = document.getElementById("PRJ1");
+            document.getElementById("viewer").innerHTML = project.innerHTML;
+            $("#BUT0").css('color', '#FEFAFF');
+            $("#BUT0").css('background-color', '#35343B');
+            $("#BUT1").css('color', '#35343B');
+            $("#BUT1").css('background-color', '#FEFAFF');
+            break;
+    }
+}
